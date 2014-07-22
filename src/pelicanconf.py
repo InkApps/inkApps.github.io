d6fc0bd45d2287960868b31f15a53387acfee97d
CkEsc <iamckesc@gmail.com>
2014-07-23 02:08:32 +0400

    Reconigured. Generated

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -10,6 +10,8 @@ TIMEZONE = 'Europe/Moscow'
 
 DEFAULT_LANG = u'ru'
 
+OUTPUT_PATH= '../'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
